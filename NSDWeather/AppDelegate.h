f54763b1430b508457d4ca3e14254b85d5bc0918
David Weldon <weldon.dave@gmail.com>
2015-09-20 16:26:04 +0100

    Initial Commit

---
+++ NSDWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDWeather
+//
+//  Created by David Weldon on 20/09/2015.
+//  Copyright (c) 2015 nsdave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
